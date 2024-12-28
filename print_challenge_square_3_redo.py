210e592f40be7c806393933200c8269603ccadfa
Dana Le <danadaole@gmail.com>
2024-12-28 17:42:13 +1030

    Print challenge square 3

---
+++ print_challenge_square_3_redo.py
@@ -0,0 +1,13 @@
+for i in range(10):
+    for j in range (10):
+        if i == 0 or i == 9:
+            print("*", end = ' ')
+        else:
+            if j == 0 or j == 9:
+              print("*", end = ' ')
+            else:
+                print(" ", end = ' ')
+    print()
+    
+    
+    
