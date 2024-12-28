3f1ff5657d546dc2a9a669f497636768d982b4fb
Dana Le <danadaole@gmail.com>
2024-12-28 17:47:31 +1030

    Print challenge square (present box)

---
+++ print_challenge_square_4_present_box.py
@@ -0,0 +1,13 @@
+for i in range(11):
+    for j in range (11):
+        if i == 0 or i == 5 or i == 10:
+            print("*", end = ' ')
+        else:
+            if j == 0 or j == 5 or j == 10:
+               print("*", end = ' ')
+            else:
+                print(" ", end = ' ')
+    print()
+    
+    
+    
